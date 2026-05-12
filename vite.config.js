c425dc5e4362f0249cae65af74e283c270067caa
Shtastliveca <dragich.trade@gmail.com>
2026-05-12 22:33:20 +0300

    First non-crashing release hopefully

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: { outDir: "dist" },
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": "http://localhost:3001",
-      "/ws": { target: "ws://localhost:3001", ws: true },
-    },
-  },
 });
